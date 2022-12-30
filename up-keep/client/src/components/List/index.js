9965c6255ec6244e5062257be244c85f631bc313
Mizuri <pro.1.h@outlook.com>
2022-12-29 20:33:55 -0700

    added some list

---
+++ up-keep/client/src/components/List/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+
+function List() {
+    const [state, dispatch] = useStoreContext();
+
+    useEffect(() => {
+        if (data) {
+            dispatch({
+                type: 
+            })
+        }
+    })
+    
+}
