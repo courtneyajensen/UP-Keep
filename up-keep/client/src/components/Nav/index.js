dec1d16fc7a600f9caf18a8e707dea8c0ec7e889
Mizuri <pro.1.h@outlook.com>
2023-01-02 19:37:42 -0700

    tailwind

--- up-keep/client/src/components/Nav/index.js
+++ up-keep/client/src/components/Nav/index.js
@@ -35,19 +35,19 @@ function Nav() {
     }
 }
 
-return (
-    <header className="flex-row px-1">
-        <h1>
-            <Link to="/">
-                <span role="img" aria-label=""></span>
-            </Link>
-        </h1>
+// return (
+//     <header className="flex-row px-1">
+//         <h1>
+//             <Link to="/">
+//                 <span role="img" aria-label=""></span>
+//             </Link>
+//         </h1>
 
-        <nav>
-            {showNavigation()}
-        </nav>
-    </header>
-);
-}
+//         <nav>
+//             {showNavigation()}
+//         </nav>
+//     </header>
+// );
+// }
 
 export default Nav;
