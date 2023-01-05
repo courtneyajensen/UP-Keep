86fd16208939d138d4feac5e6a070e28bf251ee6
Mizuri <pro.1.h@outlook.com>
2023-01-04 21:16:33 -0700

    added dashboard

--- up-keep/client/src/App.js
+++ up-keep/client/src/App.js
@@ -4,11 +4,11 @@ import './index.css';
 const App = () => 
     <div className="App text-center">
         <nav className="flex justify-between items-center p-5 bg-gradient-to-r from-teal-800 to-stone-900 text-white">
-            <h1 className="px-10 rounded">Up Keep</h1>
+            <h1 className="flex-col px-10 rounded">Up Keep</h1>
             <div className="text-lg">
-                <button className="mx-10">Login</button>
-                <button className="mx-10">Sign Up</button>
-                <button className="mx-10">Dashboard</button>
+                <button className="mx-10 border-b-2 border-b-white hover:text-sky-400">Login</button>
+                <button className="mx-10 border-b-2  border-b-white hover:text-sky-400">Sign Up</button>
+                <button className="mx-10 border-b-2  border-b-white hover:text-sky-400">Dashboard</button>
             </div>
         </nav>
     </div>
