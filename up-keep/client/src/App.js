f63b0aee3528936db0840d3bccf332aec73c0c9c
Courtney Jensen <courtneyajensen14@gmail.com>
2023-01-05 20:24:09 -0700

    LoginForm running on app.js

--- up-keep/client/src/App.js
+++ up-keep/client/src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import LoginForm from "./components/LoginForm";
 
-const App = () => 
-    <div className="App text-center">
-        <nav className="flex justify-between items-center p-5 bg-gradient-to-r from-teal-800 to-stone-900 text-white">
-            <h1 className="flex-col px-10 rounded">Up Keep</h1>
-            <div className="text-lg">
-                <button className="mx-10 border-b-2 border-b-white hover:text-sky-400">Login</button>
-                <button className="mx-10 border-b-2  border-b-white hover:text-sky-400">Sign Up</button>
-                <button className="mx-10 border-b-2  border-b-white hover:text-sky-400">Dashboard</button>
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
+
+function App(){
+    return (
+        <ApolloProvider client={client}>
+        <div className="flex w-full h-screen" >
+            <div className="w-full flex items-center justify-center lg:w-1/2">
+                <LoginForm />
             </div>
-        </nav>
-    </div>
+            <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200 ">
+                <div className="w-60 h-60 bg-gradient-to-tr from-green-700 to-orange-200 rounded-full animate-spin"></div>
+                <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg"></div>
+            </div>
+        </div>
+        </ApolloProvider>
+    );
+}
+
+
+
+//const App = () => 
+   // <div className="App text-center">
+  //      <nav className="flex justify-between items-center p-5 bg-gradient-to-r from-teal-800 to-stone-900 text-white">
+   //         <h1 className="flex-col px-10 rounded">Up Keep</h1>
+   //         <div className="text-lg">
+    //            <button className="mx-10 border-b-2 border-b-white hover:text-sky-400">Login</button>
+     //           <button className="mx-10 border-b-2  border-b-white hover:text-sky-400">Sign Up</button>
+     //           <button className="mx-10 border-b-2  border-b-white hover:text-sky-400">Dashboard</button>
+     //       </div>
+      //  </nav>
+    //</div>
 
 export default App;
---
+++ up-keep/client/src/components/LoginForm.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+export default function LoginForm() {
+    return (
+        <div className='bg-white px-10 py-20 rounded-3xl border-2 border-gray-200'>
+            <h1 className='text-5xl text-center font-bold'>Up Keep</h1>
+            <p className='font-medium text-center text-lg text-gray-500 mt-5'>Welcome to Up Keep, keeping everything in and around your home in pristine condition.</p>
+            <div className='mt-8'>
+                <div>
+                    <label className='text-lg font-medium'>Email</label>
+                    <input
+                        className='w-full border-2 border-gray-100 ounded-xl p-4 mt-1 bg-transparent'
+                        placeholder='Enter your email'
+                    />
+                </div>
+                <div>
+                    <label className='text-lg font-medium'>Password</label>
+                    <input
+                        className='w-full border-2 border-gray-100 ounded-xl p-4 mt-1 bg-transparent'
+                        placeholder='Enter your password'
+                        type="password"
+                    />
+                </div>
+                <div className='mt-8 flex justify-between items-center'>
+                    <div>
+                        <input 
+                            type="checkbox"
+                            id='remember'
+                        />
+                        <label className='ml-2 font-medium text-base ' for="remember">Rember for 30 days</label>
+                    </div>
+                    <button className='font-medium text-base text-green-700'>Forgot password</button>
+                </div>
+                <div className='mt-8 flex flex-col gap-y-4'>
+                    <button className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-green-800 text-white text-lg font-bold'>Sign in</button>
+                </div>
+                <div className='mt-8 glex justify-center items center'>
+                    <p className='font-medium text-base'>Don't have an account?</p>
+                    <button className='text-green-700'>Sign up</button>
+                </div>
+            </div>
+        </div>
+    )
+}
